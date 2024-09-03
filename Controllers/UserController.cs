eba0a41997e3d2cd269b06140c4a3b54f2525de0
reubenk97 <reubenk97@gmail.com>
2024-09-03 10:45:34 -0500

    adjusted login/reg

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Petdora.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Petdora.Controllers;
 
@@ -9,16 +10,64 @@ public class UserController : Controller
     private readonly ILogger<UserController> _logger;
     private DataContext _context;
 
-    public UserController(ILogger<UserController> logger)
+    public UserController(ILogger<UserController> logger, DataContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    [HttpGet("")]
+    public ViewResult Index() => View();
+
+    [HttpPost("users/register")]
+    public IActionResult RegisterUser(User newUser)
     {
-        return View();
+        if (!ModelState.IsValid)
+        {
+            return View("Index");
+        }
+        PasswordHasher<User> Hasher = new();
+        newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+        _context.Add(newUser);
+        _context.SaveChanges();
+
+        HttpContext.Session.SetInt32("UserId", newUser.UserId);
+
+        return RedirectToAction("Dashboard", "Post");
     }
 
+    [HttpPost("users/login")]
+    public IActionResult LoginUser(LogUser loginAttempt)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View("Index");
+        }
+        User? DbUser = _context.Users.FirstOrDefault(u => u.Email == loginAttempt.LogEmail);
+        if (DbUser == null)
+        {
+            ModelState.AddModelError("LogPassword", "Invalid credentials.");
+            return View("Index");
+        }
+        PasswordHasher<LogUser> Hasher = new();
+        PasswordVerificationResult pwCompareResult = Hasher.VerifyHashedPassword(loginAttempt, DbUser.Password, loginAttempt.LogPassword);
+        if (pwCompareResult == 0)
+        {
+            ModelState.AddModelError("LogPassword", "Invalid credentials");
+            return View("Index");
+        }
+        HttpContext.Session.SetInt32("UserId", DbUser.UserId);
+
+        return RedirectToAction("Dashboard", "Post");
+    }
+
+    [HttpPost("users/logout")]
+    public RedirectToActionResult Logout()
+    {
+        HttpContext.Session.Remove("UserId");
+        return RedirectToAction("Index");
+    }
+    
     public IActionResult Privacy()
     {
         return View();
